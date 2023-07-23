ef2f317f0d7c0b2fa211474c00d4fc8da842ff70
Anderson Albuquerque <118145194+ALBUKERKTIPS@users.noreply.github.com>
2023-07-23 01:46:47 +0100

    Adding Like Button

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 from flask_sqlalchemy import SQLAlchemy  # Allow you to manage SQL from the flask web server
 
 app = Flask(__name__)  # Variable receive the server
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/likes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/like_projects.db'
 app.config['DEBUG'] = True  # Always refresh the page, if changes
 database = SQLAlchemy(app)  # Allow you manage the database using SQLAlchemy syntax
 
 
-class CardProject(database.Model):
-    __tablename__ = 'CardProject'  # Table name of database
+class ProjectLike(database.Model):
     id = database.Column(database.Integer, primary_key=True)
-    title = database.Column(database.String(100), nullable=False)
-    description = database.Column(database.Text, nullable=False)
-    github_link = database.Column(database.String(200))
-    demo_link = database.Column(database.String(200))
-    likes = database.Column(database.Integer, default=0)  # Likes
+    title = database.Column(database.String)
+    count = database.Column(database.Integer, default=0)
+    liked_ips = database.Column(database.String)  # Stores IPs that have already been liked, separated by a comma
 
 
 with app.app_context():
@@ -24,7 +21,30 @@ with app.app_context():
 
 @app.route('/')  # Initial Route to website
 def home():
-    return render_template("index.html")
+    like_data = ProjectLike.query.filter_by(id=1).first()
+    if not like_data:
+        like_data = ProjectLike(id=1, count=0, liked_ips="")  # If there is nothing in the db ID: 0,counter:0,empty IP
+        database.session.add(like_data)
+        database.session.commit()
+
+    user_ip = request.remote_addr  # Checks if the current user has already liked
+    already_liked = user_ip in like_data.liked_ips.split(",")
+
+    return render_template("index.html", count=like_data.count, already_liked=already_liked)
 
 
-app.run()
+@app.route('/increment', methods=['POST'])
+def increment_like():
+    like_data = ProjectLike.query.filter_by(id=1).first()
+    user_ip = request.remote_addr  # Checks if the user has previously liked
+
+    if user_ip not in like_data.liked_ips.split(","):
+        like_data.count += 1
+        like_data.liked_ips += f',{user_ip}'
+        database.session.commit()
+
+    return jsonify({'count': like_data.count})
+
+
+if __name__ == '__main__':
+    app.run()
